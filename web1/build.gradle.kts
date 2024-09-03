74bb7cb9f739ef3a6651e233369e4fb8f7e9696b
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-03 21:45:58 +0300

    Refresh

--- web1/build.gradle.kts
+++ web1/build.gradle.kts
@@ -10,8 +10,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- web1/src/main/java/ru/lexender/web1/WebScript.java
+++
@@ -1,7 +0,0 @@
-package ru.lexender.web1;
-
-public class WebScript {
-    public static void main(String[] args) {
-
-    }
-}
