55892a73a5712c3846936c2994d8a21734c8854b
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-04 17:15:41 +0300

    fix: fixed workflow and deploy script

--- web1/helios-root/deploy.sh
+++ web1/helios-root/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+killall java
+
 export JAVA_VERSION=21
-httpd -f $HOME/httpd-root/conf/httpd.conf -k start
+httpd -f $HOME/httpd-root/conf/httpd.conf -k restart
 java -DFCGI_PORT=45455 -jar $HOME/httpd-root/fcgi-bin/server.jar &
 
 echo "Startup finished"
