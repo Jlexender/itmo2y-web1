b962b2cde6a889731a2fea2bf2487951104753d1
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-05 20:41:22 +0300

    fix: checkstyle

--- web1/src/main/java/ru/lexender/ifmo/web1/core/service/ContourServiceImpl.java
+++ web1/src/main/java/ru/lexender/ifmo/web1/core/service/ContourServiceImpl.java
@@ -5,12 +5,12 @@ public class ContourServiceImpl implements ContourService {
     public boolean isInsideContour(double x, double y, double r) {
         if (x >= 0) {
             if (y <= 0) {
-                return x <= r/2 && y >= -r;
+                return x <= r / 2 && y >= -r;
             } else {
-                return x*x + y*y <= r*r/4;
+                return x * x + y * y <= r * r / 4;
             }
         } else {
-            return y <= 0 && -x/2 - r/2 <= y;
+            return y <= 0 && -x / 2 - r / 2 <= y;
         }
     }
 }
