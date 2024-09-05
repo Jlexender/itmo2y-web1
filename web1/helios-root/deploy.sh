4d6b18789d766e08b34d79d2bb47a832ebd8bb84
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-05 18:34:35 +0300

    fix: deploy.sh

--- web1/helios-root/deploy.sh
+++ web1/helios-root/deploy.sh
@@ -1,8 +1,11 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
 killall java
 killall httpd
 
 export JAVA_VERSION=21
-httpd -f $HOME/httpd-root/conf/httpd.conf -k start
-nohup java -jar $HOME/webapp/fcgi-bin/server.jar > output.log 2>&1 &
+httpd -f "$HOME/httpd-root/conf/httpd.conf" -k start
+
+nohup java -DFCGI_PORT=45455 -jar "$HOME/webapp/fcgi-bin/server.jar" > "$HOME/output.log" 2>&1 &
+
+echo $! > "$HOME/server.pid"
