fd0beec095fa27f18ad54f5819f3f2a5f70c998c
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-05 18:25:25 +0300

    fix: deploy.sh

--- web1/helios-root/deploy.sh
+++ web1/helios-root/deploy.sh
@@ -5,4 +5,4 @@ killall httpd
 
 export JAVA_VERSION=21
 httpd -f $HOME/httpd-root/conf/httpd.conf -k start
-nohup java -XX:MaxHeapSize=1G -XX:MaxMetaspaceSize=128m -DFCGI_PORT=45455 -jar $HOME/webapp/fcgi-bin/server.jar > /dev/null 2>&1 &
+nohup java -jar $HOME/webapp/fcgi-bin/server.jar > output.log 2>&1 &
