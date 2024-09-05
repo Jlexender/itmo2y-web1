3a4f6e18f3928ae97f44d3de203b97c348df72bc
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-06 02:32:37 +0300

    debug: ContourService

--- web1/src/main/java/ru/lexender/ifmo/web1/core/service/ContourServiceImpl.java
+++ web1/src/main/java/ru/lexender/ifmo/web1/core/service/ContourServiceImpl.java
@@ -6,14 +6,15 @@ public class ContourServiceImpl implements ContourService {
     @Override
     public boolean isInsideContour(CoordinatesDto coordinates) {
         double x = coordinates.x(), y = coordinates.y(), r = coordinates.r();
-        if (x >= 0) {
-            if (y <= 0) {
-                return x <= r / 2 && y >= -r;
-            } else {
-                return x * x + y * y <= r * r / 4;
-            }
-        } else {
-            return y <= 0 && -x / 2 - r / 2 <= y;
-        }
+        return y >= 0;
+//        if (x >= 0) {
+//            if (y <= 0) {
+//                return x <= r / 2 && y >= -r;
+//            } else {
+//                return x * x + y * y <= r * r / 4;
+//            }
+//        } else {
+//            return y <= 0 && -x / 2 - r / 2 <= y;
+//        }
     }
 }
