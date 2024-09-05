734a236e5c6f27896bdd003d631fca063fb9390a
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-06 01:14:06 +0300

    Backend & frontend updated

--- web1/helios-root/deploy.sh
+++ web1/helios-root/deploy.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
-
-killall java
 killall httpd
 
 export JAVA_VERSION=21
 
 httpd -f $HOME/httpd-root/conf/httpd.conf -k start
-java -DFCGI_PORT=45455 -jar $HOME/webapp/fcgi-bin/server.jar &
+java -XX:MaxHeapSize=1G -XX:MaxMetaspaceSize=128m -DFCGI_PORT=45455 -jar $HOME/webapp/fcgi-bin/server.jar &
