4196ec2ddfe2a82506f0eb7fb383d4ff4f7ab6c0
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-05 18:59:03 +0300

    workflow 4

--- web1/helios-root/deploy.sh
+++ web1/helios-root/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Kill existing processes
 killall java || echo "No java process found"
 killall httpd || echo "No httpd process found"
 
-# Start httpd server
 export JAVA_VERSION=21
 
 echo "Starting httpd server..."
@@ -14,7 +12,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Start Java server with better logging
 echo "Starting Java server..."
 nohup java -DFCGI_PORT=45455 -jar "$HOME/webapp/fcgi-bin/server.jar" > "$HOME/output.log" 2> "$HOME/error.log" &
 
@@ -24,6 +21,5 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Store the process ID
 echo $! > "$HOME/server.pid"
 echo "Java server started with PID $(cat $HOME/server.pid)"
