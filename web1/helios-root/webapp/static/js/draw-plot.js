9a00b5e28903b654acdfcd842d678bacb84a3dd3
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-06 02:06:27 +0300

    upd: added label refresh on submit

--- web1/helios-root/webapp/static/js/draw-plot.js
+++ web1/helios-root/webapp/static/js/draw-plot.js
@@ -1,96 +1,99 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const center = {x: 0, y: 0};
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.translate(canvas.width / 2, canvas.height / 2);
-ctx.scale(1, -1);
+ctx.strokeStyle = 'rgba(255, 255, 255, 0.8)';
+ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
 
-const radius = 175;
+const radius = 200;
 
-function drawTicks(x, y, x1, y1, label) {
+function drawAxis() {
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x1, y1);
-    ctx.stroke();
-
-    ctx.save();
-    ctx.scale(1, -1);
-    ctx.font = '16px sans-serif';
-    ctx.fillStyle = 'gray';
-    ctx.fillText(label, x1, -y1 + 20);
-    ctx.restore();
-}
-
-const drawAxes = (rVal) => {
-    ctx.beginPath();
-    ctx.moveTo(-canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2, 0);
-    ctx.moveTo(0, -canvas.height / 2);
-    ctx.lineTo(0, canvas.height / 2);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2 - 10, 5);
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2 - 10, -5);
     ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(5, canvas.height / 2 - 10);
-    ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(-5, canvas.height / 2 - 10);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
     ctx.stroke();
-
-    if (rVal !== undefined) {
-        drawCalculatedTicks(rVal);
-        return;
-    }
-
-    drawTicks(center.x + radius, center.y, center.x + radius, center.y - 10, 'R');
-    drawTicks(center.x - radius, center.y, center.x - radius, center.y - 10, '-R');
-    drawTicks(center.x, center.y + radius, center.x + 10, center.y + radius, 'R');
-    drawTicks(center.x, center.y - radius, center.x + 10, center.y - radius, '-R');
-
-    drawTicks(center.x + radius / 2, center.y, center.x + radius / 2, center.y - 10, 'R/2');
-    drawTicks(center.x - radius / 2, center.y, center.x - radius / 2, center.y - 10, '-R/2');
-    drawTicks(center.x, center.y + radius / 2, center.x + 10, center.y + radius / 2, 'R/2');
-    drawTicks(center.x, center.y - radius / 2, center.x + 10, center.y - radius / 2, '-R/2');
-}
-
-function drawCalculatedTicks(rVal) {
-    drawTicks(center.x + radius, center.y, center.x + radius, center.y - 10, rVal);
-    drawTicks(center.x - radius, center.y, center.x - radius, center.y - 10, -rVal);
-    drawTicks(center.x, center.y + radius, center.x + 10, center.y + radius, rVal);
-    drawTicks(center.x, center.y - radius, center.x + 10, center.y - radius, -rVal);
-
-    drawTicks(center.x + radius / 2, center.y, center.x + radius / 2, center.y - 10, rVal/2);
-    drawTicks(center.x - radius / 2, center.y, center.x - radius / 2, center.y - 10, -rVal/2);
-    drawTicks(center.x, center.y + radius / 2, center.x + 10, center.y + radius / 2, rVal/2);
-    drawTicks(center.x, center.y - radius / 2, center.x + 10, center.y - radius / 2, -rVal/2);
-}
-
-const drawBroken = (data) => {
+    drawArrows();
     ctx.beginPath();
-    ctx.moveTo(data[0].x, data[0].y);
-    for (let i = 1; i < data.length; i++) {
-        ctx.lineTo(data[i].x, data[i].y);
-    }
+    ctx.arc(canvas.width / 2, canvas.height / 2, 4, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-ctx.strokeStyle = 'white';
-ctx.fillStyle = 'rgba(158,169,255,0.23)';
-drawAxes();
-drawBroken([
-    {x: -radius, y: 0},
+function drawArrows() {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width - 10, canvas.height / 2 - 5);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.lineTo(canvas.width - 10, canvas.height / 2 + 5);
+    ctx.moveTo(canvas.width / 2 - 5, 10);
+    ctx.lineTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2 + 5, 10);
+    ctx.stroke();
+}
+
+keyPoints = [
     {x: radius/2, y: 0},
     {x: radius/2, y: -radius},
     {x: 0, y: -radius},
     {x: 0, y: -radius/2},
-    {x: -radius, y: 0}
-]);
+    {x: -radius, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: radius/2}
+];
 
-ctx.beginPath();
-ctx.arc(0, 0, radius/2, 0, Math.PI/2);
-ctx.lineTo(0, 0);
-ctx.fill();
+function drawBrokenPath() {
+    ctx.moveTo(canvas.width / 2 + keyPoints[0].x, canvas.height / 2 - keyPoints[0].y);
+    for (let i = 1; i < keyPoints.length; i++) {
+        ctx.lineTo(canvas.width / 2 + keyPoints[i].x, canvas.height / 2 - keyPoints[i].y);
+    }
+}
+
+function drawArc() {
+    ctx.arc(canvas.width / 2, canvas.height / 2, radius/2, 3*Math.PI/2, 0);
+}
+
+function drawPlot() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'rgba(105, 125, 255, 0.3)';
+
+    ctx.beginPath();
+    drawAxis();
+    drawBrokenPath();
+    drawArc();
+    ctx.fill();
+
+    drawLabels();
+}
+
+labels = [
+    {x: 0, y: 0, text: '0'},
+    {x: radius/2, y: 0, text: 'R/2'},
+    {x: 0, y: -radius, text: '-R'},
+    {x: 0, y: -radius/2, text: '-R/2'},
+    {x: -radius, y: 0, text: '-R'},
+    {x: 0, y: 0, text: '0'},
+    {x: 0, y: radius/2, text: 'R/2'}
+]
+
+function drawLabels() {
+    ctx.font = '18px serif';
+    const old = ctx.fillStyle;
+    ctx.fillStyle = 'white';
+
+    for (let i = 0; i < labels.length; i++) {
+        ctx.fillText(labels[i].text, canvas.width / 2 + labels[i].x + 5, canvas.height / 2 - labels[i].y - 5);
+    }
+
+    ctx.fillStyle = old;
+}
+
+function refreshLabels(R) {
+    labels[1].text = R/2;
+    labels[2].text = -R;
+    labels[3].text = -R/2;
+    labels[4].text = -R;
+    labels[6].text = R/2;
+
+    drawPlot();
+}
+
