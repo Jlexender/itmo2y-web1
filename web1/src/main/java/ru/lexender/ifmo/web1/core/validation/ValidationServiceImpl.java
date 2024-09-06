19e615a92221ed94700d58f380c86bdce2730fd7
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-06 04:26:09 +0300

    fix:validation

--- web1/src/main/java/ru/lexender/ifmo/web1/core/validation/ValidationServiceImpl.java
+++ web1/src/main/java/ru/lexender/ifmo/web1/core/validation/ValidationServiceImpl.java
@@ -2,11 +2,16 @@ package ru.lexender.ifmo.web1.core.validation;
 
 import ru.lexender.ifmo.web1.core.dto.CoordinatesDto;
 
-public class ValidationServiceImpl implements ValidationService {
-    private static final double EPS = 1e-7;
+import java.util.List;
 
+public class ValidationServiceImpl implements ValidationService {
     @Override
     public boolean validate(CoordinatesDto coordinates) {
-        return true;
+        double x = coordinates.x(), y = coordinates.y(), r = coordinates.r();
+
+        List<Double> validX = List.of(-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0);
+        List<Double> validR = List.of(1.0, 1.5, 2.0, 2.5, 3.0);
+
+        return validX.contains(x) && validR.contains(r) && y >= -5 && y <= 3;
     }
 }
