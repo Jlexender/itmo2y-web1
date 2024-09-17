457a6f2f40797117db561fcfd922c6c7447afbf0
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-17 09:22:27 +0300

    dbg

--- web1/helios-root/webapp/static/js/fetch-data.js
+++ web1/helios-root/webapp/static/js/fetch-data.js
@@ -40,6 +40,8 @@ $(document).ready(function() {
             success: function(data) {
                 const time = new Date().getTime() - startTime;
 
+
+                alert(data)
                 const result = $(data).find('td');
 
                 const boolean = $(result).eq(0);
