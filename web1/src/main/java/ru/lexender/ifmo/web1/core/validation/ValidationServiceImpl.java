17dd9f21a70c14e10e5737e789b9f297cb08e8f4
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-17 13:40:24 +0300

    fix

--- web1/src/main/java/ru/lexender/ifmo/web1/core/validation/ValidationServiceImpl.java
+++ web1/src/main/java/ru/lexender/ifmo/web1/core/validation/ValidationServiceImpl.java
@@ -2,8 +2,6 @@ package ru.lexender.ifmo.web1.core.validation;
 
 import ru.lexender.ifmo.web1.core.dto.CoordinatesDto;
 
-import java.util.List;
-
 /**
  * Implementation of the ValidationService interface.
  * Used to check if the coordinates are valid.
@@ -16,8 +14,6 @@ public class ValidationServiceImpl implements ValidationService {
     @Override
     public boolean validate(CoordinatesDto coordinates) {
         double x = coordinates.x(), y = coordinates.y(), r = coordinates.r();
-
-
 
         return ValidationConfiguration.validX.contains(x) &&
                 ValidationConfiguration.validR.contains(r) && y >= -3 && y <= 5;
