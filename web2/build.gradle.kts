b80f7d0419de910c1aa96da83e1a05a6a4eb92ab
LLexender <aleksandrdmitryevich@gmail.com>
2024-09-27 15:48:01 +0300

    Added new repos

---
+++ web2/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("war")
+}
+
+group = "ru.lexender.ifmo.web1"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ web2/src/main/java/ru/lexender/ifmo/web2/Main.java
@@ -0,0 +1,4 @@
+package ru.lexender.ifmo.web2;
+
+public class Main {
+}
